91d84b47e5b4ed98dc2cab3ed71d149ec6ecebb1
gemtunde <gemtunde@gmail.com>
2021-04-28 04:07:01 +0100

    changes to home page

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <div class="col-lg-4 col-md-4 col-lg-oush-4 col-md-push-4">
             <div class="panel panel-default" style="margin-top:50px">
                 <div class="panel-heading">
-                    Login
+                    Login Details
                 </div>
                 <div class="panel-body">
                     <?php echo form_open('home/login_process'); ?>
